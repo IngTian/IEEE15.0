e9b215ac955df4dbb0384c8f415af9cfbd7f139e
lidl2019 <lidl200147@gmail.com>
2021-10-23 16:14:38 -0400

    remain bug for minus

--- src/mcgill/ieeextreme/oasis/expressionEvaluation/Main.java
+++ src/mcgill/ieeextreme/oasis/expressionEvaluation/Main.java
@@ -1,28 +1,31 @@
-package mcgill.ieeextreme.oasis.expressionEvaluation;
-import java.util.Arrays;
-import java.util.Scanner;
-import java.util.ArrayList;
-import java.util.Stack;
-public class Solution{
-//    class TestCase{
+package mcgill.ieeextreme.oasis.expressionEvaluation;// Don't place your source in a package
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.math.BigInteger;
+// import
+// Please name your class Main
+// package mcgill.ieeextreme.oasis.expressionEvaluation;
+class Main{
+    //    class TestCase{
 //        Stack<Integer> stack = new Stack<>();
 //    }
     public static void main(String[] args){
-//        String p = "*+2";
-        Solution k = new Solution();
-//        k.eval(p.toCharArray());
+        // String p = "*+2";
+        Main k = new Main();
+        // k.eval(p.toCharArray());
         Scanner s = new Scanner(System.in);
         int nums = s.nextInt();
 //        System.out.println(nums);
         for (int i =0; i < nums; i++){
-            char[] arr = s.next().toCharArray();
+            char[] arr = s.next().trim().toCharArray();
 //            ;
 //            String s1 = s.nextLine()
-//            System.out.println("_______");
+            // System.out.println("_______");
 //            System.out.println(s1);
             k.eval(arr);
         }
-//        System.out.println(s.nextLine());
+        // System.out.println(s.nextLine());
 //        char[] arr = s.next().toCharArray();
 //        k.eval(arr);
     }
@@ -38,7 +41,7 @@ public class Solution{
         else
             return true;
     }
-    public int eval(char[] tokens){
+    public void eval(char[] tokens){
         // Stack for numbers: 'values'
         Stack<Integer> digit = new
                 Stack<Integer>();
@@ -46,19 +49,27 @@ public class Solution{
         // Stack for Operators: 'ops'
         Stack<Character> operator = new
                 Stack<Character>();
+        if (tokens[tokens.length-1] == '+'||
+                tokens[tokens.length-1] == '-' ||
+                tokens[tokens.length-1] == '*' ||
+                tokens[tokens.length-1] == '/'){
+            System.out.println("invalid");
+            return;
+        }
         for(int i = 0; i < tokens.length; i++){
-            if (tokens[i] == ' ')
-                continue;
-            else if (tokens[i] == '('){
+            // if (tokens[i] == ' ')
+            //     continue;
+            if (tokens[i] == '('){
                 operator.push(tokens[i]);
                 count+= 1;
             }
             else if (tokens[i] >= '0' && tokens[i] <= '9'){
                 int cur_val = 0;
                 while(i < tokens.length && tokens[i] >= '0' && tokens[i] <= '9'){
-                    cur_val *= 10;
+                    cur_val *= 10 ;
                     int k = tokens[i] - '0';
                     cur_val += k;
+
                     digit.push(cur_val);
                     i++;
                 }
@@ -68,16 +79,23 @@ public class Solution{
                 count -= 1;
                 if (count < 0) {
                     System.out.println("invalid");
-                    return 0;
+                    return;
                 }
-                if (i == 0 || tokens[i-1] == '('){
+                if (i == 0 ){
                     System.out.println("invalid");
-                    return 0;
+                    return;
+                }
+                if (i >0 && tokens[i-1] == '('){
+                    System.out.println("invalid");
+                    return;
                 }
                 while (operator.peek() != '(')
-                    digit.push(applyOp(operator.pop(),
-                            digit.pop(),
-                            digit.pop()));
+                    digit.push(applyOp(operator.pop(),digit.pop(),digit.pop()));
+
+                if (operator.size() == 0) {
+                    System.out.println("invalid");
+                    return;
+                }
                 operator.pop();
             }
             else if (tokens[i] == '+' ||
@@ -85,14 +103,10 @@ public class Solution{
                     tokens[i] == '*' ||
                     tokens[i] == '/')
             {
-                // While top of 'ops' has same
-                // or greater precedence to current
-                // token, which is an operator.
-                // Apply operator on top of 'ops'
-                // to top two elements in values stack
+
                 if (i==0){
                     System.out.println("invalid");
-                    return 0;
+                    return;
                 }
                 while (!operator.empty() &&
                         hasPrecedence(tokens[i],
@@ -104,10 +118,18 @@ public class Solution{
                 // Push current token to 'ops'.
                 operator.push(tokens[i]);
             }
+            else{
+                System.out.println("invaild");
+                return;
+            }
         }
         if (count != 0){
             System.out.println("invalid");
-            return 0;
+            return;
+        }
+        if (operator.size()>digit.size()-1){
+            System.out.println("invalid");
+            return;
         }
         while (!operator.empty())
             digit.push(applyOp(operator.pop(),
@@ -116,8 +138,12 @@ public class Solution{
 
         // Top of 'values' contains
         // result, return it
-        System.out.println(digit.pop());
-        return 0;
+        int res = digit.pop();
+
+        // res = (res + 1000000007 ) %  1000000007;
+
+        System.out.println(res);
+        return;
     }
 
     public static int applyOp(char op,
@@ -126,11 +152,18 @@ public class Solution{
         switch (op)
         {
             case '+':
-                return a + b;
+                return (a + b);
             case '-':
+//                System.out.println("a _ b _ op" +op + " " + a + " " + b);
                 return a - b;
-            case '*':
+            case '*': {
+                // BigInteger bigA = new BigInteger(String.valueOf(a));
+                // BigInteger bigB = new BigInteger(String.valueOf(b));
+                // BigInteger mod = new BigInteger(String.valueOf(1000000007));
+                // BigInteger result = bigA.multiply(bigB).mod(mod);
+                // return result.intValue();
                 return a * b;
+            }
             case '/':
                 if (b == 0) {
                     System.out.println("invalid");
