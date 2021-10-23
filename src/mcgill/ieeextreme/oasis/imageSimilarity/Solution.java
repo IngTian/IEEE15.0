b040e4aaff72c85f066b4f3acea23bd8015b993a
IngTian <tzy20001018@gmail.com>
2021-10-22 21:20:13 -0400

    feat: initial commit

---
+++ src/mcgill/ieeextreme/oasis/imageSimilarity/Solution.java
@@ -0,0 +1,30 @@
+package mcgill.ieeextreme.oasis.imageSimilarity;
+
+import java.util.*;
+import java.io.*;
+
+public class Solution {
+
+    class TestCase{
+        int[][] boardA;
+        int[][] boardB;
+
+
+    }
+
+    public static void main(String[] args) {
+        // write your code here
+    }
+
+    private ArrayList<> readTestCases(){
+        Scanner scanner = new Scanner(System.in);
+
+        int numberOfTestCases = scanner.nextInt();
+        ArrayList<int[]> result = new ArrayList<>(numberOfTestCases);
+
+        for(int testCaseIndex = 0; testCaseIndex < numberOfTestCases; testCaseIndex++){
+            int numberOfRows = scanner.nextInt(), numberOfColumns = scanner.nextInt();
+            int[][] currentBoard
+        }
+    }
+}
